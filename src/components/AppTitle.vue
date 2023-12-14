82e8eb6a9aff29c83560bcbdcd05235f896ac6c5
GiuliaCarangelo <tnhkagura@gmail.com>
2023-12-14 16:19:19 +0100

    add component AppTitle

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+    msg: String,
+})
+
+</script>
+
+<template>
+    <script setup>
+        import { ref } from 'vue'
+
+        defineProps({
+            msg: String,
+        })
+
+    </script>
+
+    <template>
+    </template>
+
+    <style scoped>
+        .read-the-docs {
+            color: #888;
+        }
+    </style>
+</template>
+
+<style scoped></style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -8,7 +8,6 @@ defineProps({
 </script>
 
 <template>
-  <h1>La mia prima app con Vite! 🥸</h1>
 </template>
 
 <style scoped>
